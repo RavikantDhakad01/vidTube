050301aec0d70a558c2834bd1417ebb21410ed86
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-03-03 13:13:38 +0530

    add imports to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema({
     username: {
@@ -45,42 +47,44 @@ const UserSchema = new Schema({
 },
     { timestamps: true })
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
 
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next()
+    }
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+UserSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function () {
-    return jwt.sign(
+UserSchema.methods.genrateAccessToken = async function () {
+
+    return jwt.sign({
+        _id: this._id,
+        username: this.username
+    },
+        process.env.ACESSS_TOKEN_SECRET,
         {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName: this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: ACESSS_TOKEN_EXPIRY
         }
     )
 }
-userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign(
-        {
-            _id: this._id,
 
-        },
+UserSchema.methods.genrateRefreshToken = function () {
+
+    return jwt.sign({
+        _id: this._id,
+    },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        })
+            expiresIn: REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
+
 
 const User = mongoose.model("User", UserSchema)
 export default User
