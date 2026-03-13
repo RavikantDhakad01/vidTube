3927b1d82067be50c25641822f050e14054bc66e
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-03-13 19:21:17 +0530

    Add error log for dubging

--- src/utils/Cloudnary.js
+++ src/utils/Cloudnary.js
@@ -1,6 +1,13 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path:"./.env"
+})
+
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -13,15 +20,17 @@ const uploadOnCloudinary = async function (localFilePath) {
         if (!localFilePath) {
             return null
         }
+      
+
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-
+        
         fs.unlinkSync(localFilePath)
         return response
 
     } catch (error) {
-
+console.log("Cloudinary upload error:", error)
         if (localFilePath && fs.existsSync(localFilePath)) {
             fs.unlinkSync(localFilePath)
         }
