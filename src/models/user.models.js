e0abaa9b0919df1091aa729494bde2ada481dd29
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-03-13 19:21:48 +0530

    Fix syntax eoor

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -48,12 +48,12 @@ const UserSchema = new Schema({
     { timestamps: true })
 
 
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
     if (!this.isModified("password")) {
-        return next()
+        return
     }
     this.password = await bcrypt.hash(this.password, 10)
-    next()
+ 
 })
 
 UserSchema.methods.isPasswordCorrect = async function (password) {
