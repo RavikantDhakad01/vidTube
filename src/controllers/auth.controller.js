f003fae1cad5df36b657bd4b0681b0d2e7b471ee
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-03-13 17:17:58 +0530

    Add register user controller to vidTube

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js"
+import User from "../models/video.models.js"
+import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/Cloudnary.js"
+const registerUser = async function (req, res, next) {
+    try {
+
+        const { username, email, password, fullName } = req.body
+
+        if ([username, email, password, fullName].some((field) => field.trim() === "")) {
+            throw new ApiError(400, "All fields are required")
+        }
+
+        const existedUser = await User.findOne({
+            $or: [{ email }, { username }]
+        })
+
+        if (existedUser) {
+            throw new ApiError(409, "User with email or username already exists")
+        }
+        const avtarLocalPath = req.files?.avatar[0]?.path
+
+        if (!avtarLocalPath) {
+            throw new ApiError(400, "Avatar file is required")
+        }
+
+        let coverImageLocalPath
+
+        if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+            coverImageLocalPath = req.files?.coverImage[0]?.path
+        }
+
+        const avatar = await uploadOnCloudinary(avtarLocalPath)
+        const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+        if (!avatar) {
+            throw new ApiError(400, "Avatar file is required")
+        }
+        try {
+            const user = await User.create({
+                fullName,
+                avatar: avatar.url,
+                coverImage: coverImage?.url || "",
+                username: username.toLowerCase(),
+                email,
+                password
+            })
+
+            const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+            if (!createdUser) {
+                500, "Something went wrong while registering the user"
+            }
+
+            return res.status(201).json(new ApiResponse(201, createdUser, "user registered successfully"))
+
+        } catch (error) {
+            if (avatar) {
+                await deleteFromCloudinary(avatar.public_id)
+            }
+            if (coverImage) {
+                await deleteFromCloudinary(coverImage.public_id)
+            }
+            throw new ApiError(500, "user creation failed")
+        }
+
+    } catch (error) {
+        next(error)
+    }
+}
