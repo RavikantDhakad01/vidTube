18c235e18a5f535be7e601e3ea93cefc1c7733a1
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-05-21 09:30:56 +0530

    Add boiler plate code for tweet controller

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,42 @@
+import Tweet from "../models/tweet.models.js"
+import ApiError from "../utils/ApiError.js"
+import ApiResponse from "../utils/ApiResponse.js"
+
+const createTweet = async (req, res,next) => {
+   try {
+    
+   } catch (error) {
+    next(error)
+   }
+}
+
+const getUserTweets = async (req, res) => {
+    try {
+    
+   } catch (error) {
+    next(error)
+   }
+}
+
+const updateTweet = async (req, res) => {
+   try {
+    
+   } catch (error) {
+    next(error)
+   }
+}
+
+const deleteTweet = async (req, res) => {
+   try {
+    
+   } catch (error) {
+    next(error)
+   }
+}
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
