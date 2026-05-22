3185940b79ba64b8c1a82cc3472ea50a0f1536b0
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-05-22 18:38:47 +0530

    Add get channel stats controller

--- src/controllers/dashbord.controller.js
+++ src/controllers/dashbord.controller.js
@@ -1,10 +1,24 @@
 import ApiResponse from "../utils/ApiResponse.js"
 import ApiError from "../utils/ApiError.js"
 import Video from "../models/video.models.js"
-// TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+import Subscription from "../models/subscription.models.js"
+// TODO: Get the channel stats like total video views, total subscribers, total videos,
 const getChannelStats = async (req, res, next) => {
     try {
+        const userVideos = await Video.find({
+            owner: req.user._id
+        })
+      const  totalUserVideos = userVideos.length
 
+        const totalVideosViews = userVideos.reduce((acc, curr) => acc + curr.views, 0)
+
+        const totalSubscribersCount = await Subscription.countDocuments({
+            channel: req.user._id
+        })
+
+        return res.status(200).json(new ApiResponse(200, {
+            totalUserVideos, totalVideosViews, totalSubscribersCount
+        }, "Channel stats fetched successfully"))
     } catch (error) {
         next(error)
     }
@@ -18,7 +32,7 @@ const getChannelVideos = async (req, res, next) => {
             owner: req.user._id
         }).sort({ createdAt: -1 })
 
-        return res.status(200).json(new ApiResponse(200, userVideos, "user videos fetched successfully"))
+        return res.status(200).json(new ApiResponse(200, userVideos, "User videos fetched successfully"))
     } catch (error) {
         next(error)
     }
