a93a6f3c20fd85fb0cd08b37a6277fe8fd24e65b
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-05-22 09:19:38 +0530

    Add update tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -18,12 +18,12 @@ const createTweet = async (req, res, next) => {
       next(error)
    }
 }
-const getUserTweets = async (req, res,next) => {
+const getUserTweets = async (req, res, next) => {
    const { userId } = req.params
    try {
       const userTweets = await Tweet.find({
          owner: userId
-      }).sort({createdAt:-1})
+      }).sort({ createdAt: -1 })
       return res.status(200).json(new ApiResponse(200, userTweets, "user tweets fetched successfully"))
    } catch (error) {
       next(error)
@@ -31,15 +31,32 @@ const getUserTweets = async (req, res,next) => {
 }
 
 
-const updateTweet = async (req, res) => {
+const updateTweet = async (req, res, next) => {
+   const { tweetId } = req.params
+   const { content } = req.body
    try {
+      if (!content || content.trim() === "") {
+         throw new ApiError(400, "Fields are required")
+      }
+
+      const tweet = await Tweet.findById(tweetId)
+      if (!tweet) {
+         throw new ApiError(404, "Tweet does not exist")
+      }
+
+      if (tweet.owner.toString() !== req.user._id.toString()) {
+         throw new ApiError(403, "Unauthorized access")
+      }
+      tweet.content = content.trim()
+      await tweet.save({ validateBeforeSave: false })
+      return res.status(200).json(new ApiResponse(200, tweet, "Tweet updated successfully"))
 
    } catch (error) {
       next(error)
    }
 }
 
-const deleteTweet = async (req, res) => {
+const deleteTweet = async (req, res, next) => {
    try {
 
    } catch (error) {
