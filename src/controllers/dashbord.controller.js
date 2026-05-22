05bc34c709584d5f507babf6cce63511b671380d
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-05-22 18:15:37 +0530

    Add get user videos controller

--- src/controllers/dashbord.controller.js
+++ src/controllers/dashbord.controller.js
@@ -1,6 +1,6 @@
 import ApiResponse from "../utils/ApiResponse.js"
 import ApiError from "../utils/ApiError.js"
-
+import Video from "../models/video.models.js"
 // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 const getChannelStats = async (req, res, next) => {
     try {
@@ -14,7 +14,11 @@ const getChannelStats = async (req, res, next) => {
 const getChannelVideos = async (req, res, next) => {
 
     try {
+        const userVideos = await Video.find({
+            owner: req.user._id
+        }).sort({ createdAt: -1 })
 
+        return res.status(200).json(new ApiResponse(200, userVideos, "user videos fetched successfully"))
     } catch (error) {
         next(error)
     }
