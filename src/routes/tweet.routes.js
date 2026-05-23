eebe219bc79472897dd9236ede94a82ce94883c0
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-05-23 07:01:08 +0530

    Add tweet routes

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+   createTweet,
+   getUserTweets,
+   updateTweet,
+   deleteTweet
+} from "../controllers/tweet.controller.js"
+
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+router.route("/").post(verifyJwt,createTweet)
+router.route("/:userId").post(verifyJwt,getUserTweets)
+router.route("/:tweetId").patch(verifyJwt,updateTweet).delete(verifyJwt,deleteTweet)
+
+
+export default router
